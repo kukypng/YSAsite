c3659539d5632be2d278b9999fb174cd096d7b3a
kuky <86914993+kukypng@users.noreply.github.com>
2025-02-17 17:44:36 -0300

    yyy

--- ysa/ysa.js
+++ ysa/ysa.js
@@ -143,11 +143,9 @@ function confirmarPedido() {
     mensagem += `*Total: R$ ${totalComEntrega},00*\n`;
     mensagem += `Endereço: ${metodoEntrega === 'retirada' ? 'Retirada no local' : endereco}\n`;
     mensagem += `Forma de Pagamento: ${pagamento}*\n`;
-  mensagem += `Obrigado pela preferencia 😊\n `;
   
     if (pagamento === 'dinheiro') {
         mensagem += `Troco para: R$ ${troco},00\n`;
-        mensagem += `Obrigado pela preferencia 😊\n`;
         mensagem += `_Cantinho Da Ysa_\n`;
     }
 
